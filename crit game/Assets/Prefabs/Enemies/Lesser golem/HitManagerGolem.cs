50dc5bb32ba5b01e2868b0db7d79090e5ac16f9c
wadsadasdw1 <davidbosch2006@outlook.com>
2025-10-02 21:42:32 +0200

    Golems work?
    player transform field in the golem chase script needs to be manually set each time a new golem is instantiated

--- crit game/Assets/Prefabs/Enemies/Lesser golem/HitManagerGolem.cs
+++ crit game/Assets/Prefabs/Enemies/Lesser golem/HitManagerGolem.cs
@@ -1,17 +1,18 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HitManagerGolem : MonoBehaviour
 {
     private GolemValues golemValues;
-
+    [SerializeField] public GameObject damageEffect;
     private void Start()
     {
         golemValues = GetComponent<GolemValues>();
     }
     public void TakeDamage(float damage)
     {
-        Debug.Log(damage);
+        Instantiate(damageEffect, new Vector3(transform.position.x, transform.position.y + 2.044939f, transform.position.z), Quaternion.identity);
         golemValues.currentHealth = golemValues.currentHealth - damage;
     }
 
